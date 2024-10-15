dc002b98c0627aff25fbef9d1e20a56962d20634
Kichuprobotzzz <111451450+Kichuprobotzzz@users.noreply.github.com>
2024-10-15 20:46:28 +0530

    Update video.py

--- video.py
+++ video.py
@@ -188,7 +188,7 @@ async def upload_video(client, file_path, thumbnail_path, video_title, reply_msg
         await message.delete()
 
     await reply_msg.delete()
-    sticker_message = await message.reply_sticker("CAACAgIAAxkBAAEZdwRmJhCNfFRnXwR_lVKU1L9F3qzbtAAC4gUAAj-VzApzZV-v3phk4DQE")
+    sticker_message = await message.reply_sticker("CAACAgUAAxkBAAEM-DFnDocHxzzdxA3pGXrVH98mvH-u7QAClxMAAj3xcVS7F_0jw0cQTjYE")
     os.remove(file_path)
     os.remove(thumbnail_path)
     await asyncio.sleep(5)
