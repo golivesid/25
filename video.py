5b5ed7c9c4608e470723851f5cb384963a1f67d1
KichuTG <kichumon333@outlook.com>
2024-11-10 09:38:36 +0530

    Update video.py

--- video.py
+++ video.py
@@ -174,12 +174,14 @@ async def upload_video(client, file_path, thumbnail_path, video_title, reply_msg
 
     with open(file_path, 'rb') as file:
         collection_message = await client.send_video(
-            chat_id=collection_channel_id,
-            video=file,
-            caption=f"✨ {video_title}\n👤 ʟᴇᴇᴄʜᴇᴅ ʙʏ : {user_mention}\n📥 ᴜsᴇʀ ʟɪɴᴋ: tg://user?id={user_id}",
-            thumb=thumbnail_path,
-            progress=progress
-            (reply_markup =  InlineKeyboardMarkup([[ InlineKeyboardButton("ᴍᴏʀᴇ ᴠɪᴅᴇᴏꜱ", switch_inline_query_current_chat="text"), ]])
+    chat_id=collection_channel_id,
+    video=file,
+    caption=f"✨ {video_title}\n👤 ʟᴇᴇᴄʜᴇᴅ ʙʏ : {user_mention}\n📥 ᴜsᴇʀ ʟɪɴᴋ: tg://user?id={user_id}",
+    thumb=thumbnail_path,
+    progress=progress,
+    reply_markup=InlineKeyboardMarkup([
+        [InlineKeyboardButton("ᴍᴏʀᴇ ᴠɪᴅᴇᴏꜱ", switch_inline_query_current_chat="text")]
+    ])
         )
         await client.copy_message(
             chat_id=message.chat.id,
